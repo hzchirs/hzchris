7e22740dc5519351b473edc0bb83ceb7e45b075c
HzChris <hzchirs@gmail.com>
2019-11-14 21:43:36 +0800

    Regular update on: 2019-11-14

---
+++ backup_to_github.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+  cd ~/Projects/hzchris
+  echo "Following files will be updated!"
+  git status -s
+  git add --all
+  git commit -m "Regular update on: `date +%Y-%m-%d`"
+  git push
+fi
+  
