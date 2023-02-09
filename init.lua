5807c90a091191578ce9856049425f9ebbaa8a39
HzChris <hzchirs@gmail.com>
2023-02-09 16:00:00 +0800

    Regular update on: 2023-02-09

--- init.lua
+++ init.lua
@@ -396,7 +396,6 @@ require("lazy").setup({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
