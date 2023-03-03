25661c5d06e417f15371b0df922683c56fc313dc
HzChris <hzchirs@gmail.com>
2023-03-03 23:00:00 +0800

    Regular update on: 2023-03-03

--- init.lua
+++ init.lua
@@ -257,28 +257,28 @@ require("lazy").setup({
     end
   },
 
-  { 
-    'williamboman/mason.nvim',
-    dependencies = {
-      'williamboman/mason-lspconfig.nvim'
-    },
-    config = function()
-      require("mason").setup({
-        ui = {
-          border = 'single',
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-          }
-        }
-      })
-
-      require("mason-lspconfig").setup({
-        automatic_installation = true
-      })
-    end
-  },
+  -- { 
+  --   'williamboman/mason.nvim',
+  --   dependencies = {
+  --     'williamboman/mason-lspconfig.nvim'
+  --   },
+  --   config = function()
+  --     require("mason").setup({
+  --       ui = {
+  --         border = 'single',
+  --         icons = {
+  --           package_installed = "✓",
+  --           package_pending = "➜",
+  --           package_uninstalled = "✗"
+  --         }
+  --       }
+  --     })
+  --
+  --     require("mason-lspconfig").setup({
+  --       automatic_installation = true
+  --     })
+  --   end
+  -- },
   {
     'neovim/nvim-lspconfig',
     config = function()
