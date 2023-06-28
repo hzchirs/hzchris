00a47678b8b856dd6269b206dd0539ca49a8ba13
HzChris <hzchirs@gmail.com>
2023-06-28 11:00:00 +0800

    Regular update on: 2023-06-28

--- init.lua
+++ init.lua
@@ -612,6 +612,25 @@ require("lazy").setup({
       require('leap').add_default_mappings()
     end
   },
+  -- ipython, jupyter
+  {
+    "WhiteBlackGoose/magma-nvim-goose",
+    version = "*",
+    build = ":UpdateRemotePlugins",
+    keys = {
+      { "<leader>mi", "<cmd>MagmaInit<CR>", desc = "This command initializes a runtime for the current buffer." },
+      { "<leader>mo", "<cmd>MagmaEvaluateOperator<CR>", desc = "Evaluate the text given by some operator." },
+      { "<leader>ml", "<cmd>MagmaEvaluateLine<CR>", desc = "Evaluate the current line." },
+      { "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>", desc = "Evaluate the selected text." },
+      { "<leader>mc", "<cmd>MagmaEvaluateOperator<CR>", desc = "Reevaluate the currently selected cell." },
+      { "<leader>mr", "<cmd>MagmaRestart!<CR>", desc = "Shuts down and restarts the current kernel." },
+      {
+        "<leader>mx",
+        "<cmd>MagmaInterrupt<CR>",
+        desc = "Interrupts the currently running cell and does nothing if not cell is running.",
+      },
+    },
+  },
 
   -- 一直有 bug，先暫時註解，改用 leap.nvim
   -- {
