17225ab4ffcd78dd10c95b805579f2994031d3b0
HzChris <hzchirs@gmail.com>
2023-07-22 11:00:01 +0800

    Regular update on: 2023-07-22

--- init.lua
+++ init.lua
@@ -426,12 +426,20 @@ require("lazy").setup({
         suggestion = {
           auto_trigger = true,
           keymap = {
-            accept = "<Tab>",
+            accept = false,
             next = "<A-]>",
             prev = "<A-[>",
           }
         },
       })
+
+      vim.keymap.set('i', '<Tab>', function()
+        if require("copilot.suggestion").is_visible() then
+          require("copilot.suggestion").accept()
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+        end
+      end, { desc = "Super Tab" })
     end
   },
 
