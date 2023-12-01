499784ff15f8641b1d0401de7ff80cf9740e91f3
HzChris <hzchirs@gmail.com>
2023-12-01 12:00:01 +0800

    Regular update on: 2023-12-01

--- init.lua
+++ init.lua
@@ -424,31 +424,34 @@ require("lazy").setup({
 
   -- copilot
   {
-    'zbirenbaum/copilot.lua',
-    config = function()
-      require("copilot").setup({
-        panel = {
-          auto_refresh = false,
-        },
-        suggestion = {
-          auto_trigger = true,
-          keymap = {
-            accept = false,
-            next = "<A-]>",
-            prev = "<A-[>",
-          }
-        },
-      })
-
-      vim.keymap.set('i', '<Tab>', function()
-        if require("copilot.suggestion").is_visible() then
-          require("copilot.suggestion").accept()
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-        end
-      end, { desc = "Super Tab" })
-    end
+    'github/copilot.vim'
   },
+  -- {
+  --   'zbirenbaum/copilot.lua',
+  --   config = function()
+  --     require("copilot").setup({
+  --       panel = {
+  --         auto_refresh = false,
+  --       },
+  --       suggestion = {
+  --         auto_trigger = true,
+  --         keymap = {
+  --           accept = false,
+  --           next = "<A-]>",
+  --           prev = "<A-[>",
+  --         }
+  --       },
+  --     })
+  --
+  --     vim.keymap.set('i', '<Tab>', function()
+  --       if require("copilot.suggestion").is_visible() then
+  --         require("copilot.suggestion").accept()
+  --       else
+  --         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  --       end
+  --     end, { desc = "Super Tab" })
+  --   end
+  -- },
 
   -- buffer management
   {
